75278e168860b302fcd881dfbc50c278b093a7b2
Maria Go <merigon_93@hotmail.com>
2022-07-18 10:37:03 +0100

    second exercise

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Scene from '../src/components/scene/Scene'
+import Data from './Data'
 
 export default function App() {
+  const newData = Data.map(d => {
+    return (
+      <Scene 
+        d={d}
+      />
+    )
+  })
   return(
     <div>
-      <Scene />
+      {newData}
     </div>
   )
 }
--- src/components/scene/Scene.js
+++ src/components/scene/Scene.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function Scene() {
+export default function Scene(props) {
   return (
-    <p> El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial </p>
+    <div>
+      <p>{props.d.quote}</p>
+    </div>
   )
 }
