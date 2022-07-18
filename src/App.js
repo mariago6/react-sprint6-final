bb04dc3bab66013ff39e7a88793ce1c4bba8f9e1
Maria Go <merigon_93@hotmail.com>
2022-07-18 11:13:53 +0100

    third exercise

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import Scene from '../src/components/scene/Scene'
-import Data from './Data'
+import React from 'react';
+import Scene from '../src/components/scene/Scene'; 
+import Data from './Data'; 
+import {Border} from './styled'
 
 export default function App() {
   const newData = Data.map(d => {
     return (
-      <Scene 
-        d={d}
-      />
+      <div> 
+        <Border>
+          <Scene 
+            d={d}
+          />
+        </Border>
+        
+      </div>
+      
     )
-  })
+  }); 
   return(
     <div>
       {newData}
---
+++ src/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'; 
+
+export const Border = styled.div`
+  background-color: transperent;
+  padding-top: 5px;
+  padding-bottom: 5px; 
+  margin: 15px; 
+  border: solid 2px; 
+  border-radius: 35px; 
+  border-color: black;  
+  text-align: center; 
+`; 
